29351a52f55701787d832d0a853857da80c767d5
sajjadhza <sa1ho3za77@gmail.com>
2024-04-09 10:37:21 +0330

    ADD : SWiPER

--- src/Components/pages/HomePage.js
+++ src/Components/pages/HomePage.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 import Header from "../wedgits/Header";
 import Footer from "../wedgits/Footer";
 import "../css/HomePage.scss";
+import Slider from "./Slider";
+import Swaper from "./Swaper";
 
 function HomePage(props) {
-
-
-    return (
-        <div className="mainContainer">
-            <Header/>
-            <div className="mainContent">
-                <div className="bannerContainer">
-
-                </div>
-                <div className="crucelContainer">
-
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div className="mainContainer">
+      <Header />
+      <div className="mainContent">
+        <div className="bannerContainer">
+        <Slider />
         </div>
-    );
+        <div className="crucelContainer">
+       <Swaper/>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ src/Components/pages/Slider.js
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import imgslider from "../css/images/f9f2d9c459c927461e22e5468465e638.jpg";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../css/Slider.scss";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+export default function Slider() {
+  return (
+    <>
+      <div className="main-container-slider">
+        <Swiper
+          cssMode={true}
+          navigation={true}
+          pagination={true}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className="slider"
+        >
+          <SwiperSlide>
+            <img className="img-slider" src={imgslider} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="img-slider" src={imgslider} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="img-slider" src={imgslider} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="img-slider" src={imgslider} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="img-slider" src={imgslider} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+}
